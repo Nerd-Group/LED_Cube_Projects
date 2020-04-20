27b36989699aad033605682ab42dc9c6e2750628
gjm <gjm>
2020-04-20 18:28:04 +0200

    Random: Update the firmware for an 8x8x8 Cube

--- Random blinking light/tinyCube.ino
+++ Random blinking light/tinyCube.ino
@@ -3,21 +3,28 @@
  * Store Pin: Pin 1
  * Data Pin: Pin 2
 */
+#define shiftPin 0 // this is the ClockPin
+#define storePin 1 
+#define datenPin 2 //datePin seems to be allready in use for shiftOut
+#define resetPin 3 // resetPin is till now not in use
 
-unsigned short Farben[4] = {0};
-unsigned long lastAnimStep = 0;
+#define LEDSize 8 //The Maximum of LEDs is 8
+
+
+unsigned short Farben[LEDSize] = {0};
+unsigned long lastAnimStep = 0; //rewrite with a boolean array.
 
 
 void setup() {
   
   // set all pins to output
   
-  pinMode(0, OUTPUT);
-  pinMode(1, OUTPUT);
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
+  pinMode(shiftPin, OUTPUT);
+  pinMode(storePin, OUTPUT);
+  pinMode(datenPin, OUTPUT);
+  pinMode(resetPin, OUTPUT);
 
-  digitalWrite(3, LOW);
+  digitalWrite(resetPin, LOW); //TODO reset if low or High?
 }
 
 void loop() {
@@ -28,9 +35,9 @@ void loop() {
     lastAnimStep = millis();
 
     // generate all 4 areas
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < LEDSize; i++)
     {
-      Farben[i] = random(65535);
+      Farben[i] = random(pow(2,LEDSize*LEDSize)); //generates all Posible states of one Stage (LED on or off)
       //Farben[i] = 65535;
 
     }
@@ -38,27 +45,32 @@ void loop() {
 
 
   // Display on the LED Cube
-  for (int e = 0; e < 4; e++)
+  for (int e = 0; e < LEDSize; e++)
   {
     int e_activation = 0;
     switch (e)
     {
-      case 0: e_activation = 0b00000001; break;
+      case 0: e_activation = 0b00000001; break; //long is 32 bit... not 8
       case 1: e_activation = 0b00000010; break;
       case 2: e_activation = 0b00000100; break;
       case 3: e_activation = 0b00001000; break;
+      case 4: e_activation = 0b00010000; break;
+      case 5: e_activation = 0b00100000; break;
+      case 6: e_activation = 0b01000000; break;
+      case 7: e_activation = 0b10000000; break;
     }
     
     // shift the data out
-    digitalWrite(1, LOW);
-    shiftOut(2, 0, MSBFIRST, e_activation);
-    shiftOut(2, 0, MSBFIRST, Farben[e] >> 8);
-    shiftOut(2, 0, MSBFIRST, Farben[e]);
+    digitalWrite(storePin, LOW);
+    shiftOut(datenPin, shiftPin, MSBFIRST, e_activation);
+    shiftOut(datenPin, shiftPin, MSBFIRST, Farben[e] >> 8); //Todo muessen hier jetzt 16 geschoben werden? Bisher haetten doch eigentlich nur 4 geschoben werden muessen
+    shiftOut(datenPin, shiftPin, MSBFIRST, Farben[e]);
     digitalWrite(1, HIGH);
 
-    digitalWrite(3, LOW);// Output Enable activate
+//TODO ist hier wirklich der ResetPin gemeint?
+    digitalWrite(resetPin, LOW);// Output Enable activate
     delay(2);//display the area for one millisecond
-    digitalWrite(3, HIGH);// Output Enable deactivate
+    digitalWrite(resetPin, HIGH);// Output Enable deactivate
   }
 }
 
